6dc4ceb7628540fd99d71e85b0497f306cc5f823
Jota Uribe <juribee@unicartagena.edu.co>
2018-05-21 20:56:42 -0500

    first commit

---
+++ src/app/authService.js
@@ -0,0 +1,27 @@
+const BearerStrategy = require('passport-http-bearer').Strategy
+const Token = require('../models/token');
+
+passport.use(new BearerStrategy(
+  function(accessToken, callback) {
+    Token.findOne({value: accessToken }, function (err, token) {
+      if (err) { return callback(err); }
+
+      // No token found
+      if (!token) { return callback(null, false); }
+
+      User.findOne({ _id: token.userId }, function (err, user) {
+        if (err) { return callback(err); }
+
+        // No user found
+        if (!user) { return callback(null, false); }
+
+        // Simple example with no scope
+        callback(null, user, { scope: '*' });
+      });
+    });
+  }
+));
+
+...
+
+exports.isBearerAuthenticated = passport.authenticate('bearer', { session: false });
